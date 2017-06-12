0ddd673d7f38b88aea066ebe9732ba3ea4391ae2
Verity <vhonebon@gmail.com>
2017-06-12 21:05:54 +0100

    Added low balance method to top_up unit test and passed it

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,32 +1,33 @@
 class Oystercard
-	
+
 	CARD_LIMIT = 90
 
 	attr_accessor :balance, :in_use
-	
+
 	def initialize(balance = 0, in_use = false)
 		@balance = balance
 		@in_use = in_use
-	end 
-	
+	end
+
 	def top_up(amount)
-		fail "Can't top up above £#{CARD_LIMIT}" unless @balance + amount <= CARD_LIMIT 
-		@balance  += amount
+		fail "Can't top up above £#{CARD_LIMIT}" unless @balance + amount <= CARD_LIMIT
+		@balance += amount
 	end
 
 	def deduct(amount)
 		@balance -= amount
-	end 
+	end
 
 	def touch_in
+		fail "Not enough credit - please top up!" unless @balance > 1
 		@in_use = true
 	end
 
-	def in_journey? 
-		@in_use == true 
-	end 
+	def in_journey?
+		@in_use == true
+	end
 
 	def touch_out
 		@in_use = false
-	end 
+	end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -18,29 +18,40 @@ describe Oystercard do
 			expect{card.top_up(1)}.to raise_error("Can't top up above £#{Oystercard::CARD_LIMIT}")
 		end
 	end
+
 	describe "#deduct" do
 		it "deducts some amount from the card balance" do
 			random_amount = rand(1..90)
 			card.top_up(random_amount)
-			expect(card.deduct(random_amount)).to eq 0 
+			expect(card.deduct(random_amount)).to eq 0
 		end
 	end
+
 	describe "#touch_in" do
+		context "Where there is greater than £1 credit on card" do
 		it "changes card status to in use when touched in" do
 			card.top_up(10)
 			expect(card.touch_in).to eq(true)
 		end
 	end
+		context "when card has less than £1 credit" do
+			it "raises an error" do
+			expect{card.touch_in}.to raise_error("Not enough credit - please top up!")
+		end
+	end
+end
+
 	describe "#in_journey?" do
 		it "tells whether a card is in use" do
-			#allow(card).to receive_message_chain(:top_up, :touch_in, :in_use).and_return(true)
 			card.top_up(10)
 			card.touch_in
 			expect(card.in_journey?).to eq(true)
 		end
 	end
+
 	describe "#touch_out" do
 		it "changes card status to not in use" do
+			card.top_up(10)
 			card.touch_in
 			card.touch_out
 			expect(card).not_to be_in_journey
