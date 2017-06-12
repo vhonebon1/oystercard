800cf9d19ecab4f7460706a0e2a1c46eb0ce4c60
terminalobject <doctorboredom@hotmail.com>
2017-06-12 15:47:34 +0100

    first 5 steps

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,13 @@
 class Oystercard
+	
+	attr_accessor :balance
+	
+	def initialize
+		@balance = 0
+	end 
+	
+	def top_up(amount)
+		self.balance  = self.balance + amount
+	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,15 @@
 require './lib/oystercard.rb'
 describe Oystercard do
-	
+	it "expects the card to have a balance of 0 when initialized" do 
+		card = Oystercard.new 
+		expect(card.balance).to eq 0
+	end 
+
+	describe "#top_up" do 
+		it "increases the card balance by a specified amount" do 
+			card = Oystercard.new
+			card.top_up(5)
+			expect(card.balance).to eq 5 
+		end 
+	end 
 end
