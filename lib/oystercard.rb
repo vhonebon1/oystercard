ff4c3ca1b12499c21c003d0b713c0aa2276dc753
Verity <vhonebon@gmail.com>
2017-06-12 16:48:34 +0100

    Added and passed unit test for touch in method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,18 +2,24 @@ class Oystercard
 	
 	CARD_LIMIT = 90
 
-	attr_accessor :balance
+	attr_accessor :balance, :in_use
 	
 	def initialize
 		@balance = 0
+		@in_use = false
 	end 
 	
 	def top_up(amount)
 		fail "Total balance cannot exceed £#{CARD_LIMIT}" unless self.balance + amount <= CARD_LIMIT 
 		self.balance  = self.balance + amount
 	end
+
 	def deduct(amount)
 		self.balance = self.balance - amount
 	end 
+
+	def touch_in
+		in_use = true
+	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,29 +1,34 @@
 require './lib/oystercard.rb'
 describe Oystercard do
-	it "expects the card to have a balance of 0 when initialized" do 
-		card = Oystercard.new 
+
+	let(:card) {described_class.new}
+
+	it "expects the card to have a balance of 0 when initialized" do  
 		expect(card.balance).to eq 0
 	end 
 
 	describe "#top_up" do 
 		it "increases the card balance by a specified amount" do 
-			card = Oystercard.new
 			card.top_up(5)
 			expect(card.balance).to eq 5 
 		end 
 		context "If top up makes balance exceed limit of £90"
 		it "raises an error" do
-			card = Oystercard.new
 			card.top_up(Oystercard::CARD_LIMIT)
 			expect{card.top_up(1)}.to raise_error("Total balance cannot exceed £90")
 		end
 	end 
 	describe "#deduct" do 
 		it "deducts some amount from the card balance" do
-			card = Oystercard.new
 			random_amount = rand(1..90)
 			card.top_up(random_amount)
 			expect(card.deduct(random_amount)).to eq 0 
 		end 
 	end 
+	describe "#touch_in" do
+		it "changes card status to in use when touched in" do
+			card.top_up(10)
+			expect(card.touch_in).to eq(true)
+		end
+	end
 end
