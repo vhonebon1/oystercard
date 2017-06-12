1f988844d7061c4719fb9210545b4e9a30199baa
Verity <vhonebon@gmail.com>
2017-06-12 19:03:47 +0100

    Made amendment to top up error

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,28 +3,28 @@ describe Oystercard do
 
 	let(:card) {described_class.new}
 
-	it "expects the card to have a balance of 0 when initialized" do  
+	it "expects the card to have a balance of 0 when initialized" do
 		expect(card.balance).to eq 0
-	end 
+	end
 
-	describe "#top_up" do 
-		it "increases the card balance by a specified amount" do 
+	describe "#top_up" do
+		it "increases the card balance by a specified amount" do
 			card.top_up(5)
-			expect(card.balance).to eq 5 
-		end 
+			expect(card.balance).to eq 5
+		end
 		context "If top up makes balance exceed limit of £90"
 		it "raises an error" do
 			card.top_up(Oystercard::CARD_LIMIT)
-			expect{card.top_up(1)}.to raise_error("Total balance cannot exceed £90")
+			expect{card.top_up(1)}.to raise_error("Can't top up above £#{Oystercard::CARD_LIMIT}")
 		end
-	end 
-	describe "#deduct" do 
+	end
+	describe "#deduct" do
 		it "deducts some amount from the card balance" do
 			random_amount = rand(1..90)
 			card.top_up(random_amount)
 			expect(card.deduct(random_amount)).to eq 0 
-		end 
-	end 
+		end
+	end
 	describe "#touch_in" do
 		it "changes card status to in use when touched in" do
 			card.top_up(10)
@@ -37,13 +37,13 @@ describe Oystercard do
 			card.top_up(10)
 			card.touch_in
 			expect(card.in_journey?).to eq(true)
-		end 
-	end 
+		end
+	end
 	describe "#touch_out" do
 		it "changes card status to not in use" do
 			card.touch_in
 			card.touch_out
 			expect(card).not_to be_in_journey
-		end 
-	end 
+		end
+	end
 end
